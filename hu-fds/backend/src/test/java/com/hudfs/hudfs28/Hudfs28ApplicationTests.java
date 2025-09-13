f8fed512982d7581a2acf99518d3b94baf902e2d
znisakaratas <162369517+znisakaratas@users.noreply.github.com>
2025-09-13 17:14:16 +0300

    Add files via upload

---
+++ hu-fds/backend/src/test/java/com/hudfs/hudfs28/Hudfs28ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hudfs.hudfs28;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hudfs28ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
